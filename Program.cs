175059bb0604263dcfa70450671864d62f175ae1
Daniel Coady <daniel.coady@outlook.com.au>
2018-08-19 17:01:14 +1000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace CLIClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock myClock = new Clock();
+
+            while (true)
+	        {
+		        myClock.Tick();
+		        Console.WriteLine("Current time: {0}", myClock.CurrentTime);
+		        Thread.Sleep(1000); // Sleep for one second
+	        }
+        }
+    }
+}
